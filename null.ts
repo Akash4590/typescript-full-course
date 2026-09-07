8bc8e90be77c71691de6f4b65efd4d546027dff1
Akash4590 <akashjaved4590@gmail.com>
2026-09-07 12:21:52 +0500

    null and undefined added

---
+++ null.ts
@@ -0,0 +1,14 @@
+// var data:null=null;
+// var val:undefined=undefined;
+// console.log(val)
+
+// var data:null|string=null;
+// var username:null|string=null;
+// var login= true;
+// if(login){
+//     username="Akash"          
+// }
+// console.log(typeof data)
+
+var item:undefined=undefined;
+console.log(item);
