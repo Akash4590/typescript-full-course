2f5d89f4ed8224b9eb7c1dbb4a6f11e211c8ff02
Akash4590 <akashjaved4590@gmail.com>
2026-09-08 11:46:25 +0500

    array and tuple added

---
+++ Array.js
@@ -0,0 +1,6 @@
+"use strict";
+var users = ["Akash", "khan", "mohsin"];
+var marks = [34, 54, 56, 67];
+console.log(users);
+users.push("Ali");
+//# sourceMappingURL=Array.js.map
---
+++ Array.ts
@@ -0,0 +1,18 @@
+// var users: string[] = ["Akash", "khan", "mohsin"];
+
+// var marks: number[] = [34, 54, 56, 67];
+
+// users.push("Ali");
+// marks.push(10);
+// console.log(marks)
+// console.log(users);
+
+
+
+// ==============Tuple===============
+
+var empty: string[] = ["anil", "Akash", "khan"];
+
+var empdata: [string, number, boolean] = ["Akash", 30, true];
+
+console.log(empdata);
